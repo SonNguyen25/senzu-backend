af17be348b424f8f2d1132ae412900dbdaecad5c
sonnguyen25 <ssbaoson@gmail.com>
2025-03-23 02:59:28 -0400

    fixing merge

--- App.js
+++ App.js
@@ -4,41 +4,22 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import session from "express-session";
+import FriendRoutes from "./Friends/routes.js";
 import ChatRoutes from "./OpenAI/routes.js";
 import UsersRoutes from "./Users/routes.js";
 import PostRoutes from "./Posts/routes.js";
 import CommentRoutes from "./Comments/routes.js";
 import FollowRoutes from "./Follow/routes.js";
-import FriendRoutes from "./Friends/routes.js";
 
-
-const app = express();
-const corsOptions = {
-    credentials: true,
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000", process.env.HTTP_SERVER_DOMAIN],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type",
-    "Authorization",
-    "Accept",
-    "Origin",
-    "X-Requested-With",
-    "X-CSRF-Token",
-    "Cache-Control",
-    "Pragma",
-    "If-None-Match",
-    "If-Modified-Since"],
-};
-app.use(cors(corsOptions));
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/senzu";
-mongoose.connect(CONNECTION_STRING,
-    // { useNewUrlParser: true,
-    //     useUnifiedTopology: true,
-    //     tls: true, }
-    );
-
+mongoose.connect(CONNECTION_STRING);
+const app = express();
 // console.log(process.env.OPENAI_API_KEY);
-
+app.use(cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+}));
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
@@ -59,11 +40,10 @@ app.use(
 );
 Hello(app);
 ChatRoutes(app);
- UsersRoutes(app);
- FriendRoutes(app);
- FollowRoutes(app);
- 
- PostRoutes(app);
- CommentRoutes(app);
-app.listen(process.env.PORT || 4000);
+UsersRoutes(app);
+FriendRoutes(app);
+FollowRoutes(app);
 
+PostRoutes(app);
+CommentRoutes(app);
+app.listen(process.env.PORT || 4000);
