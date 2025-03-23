65da2d3a9394347ead22ce91aef2d717f3eaa6c3
sonnguyen25 <ssbaoson@gmail.com>
2025-03-23 03:16:13 -0400

    fix cors

--- App.js
+++ App.js
@@ -36,6 +36,18 @@ if (process.env.NODE_ENV !== "development") {
     };
 }
 app.use('/uploads', express.static('uploads'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://senzusocial.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
+    next();
+  });
 app.use(express.json());
 app.use(
     session(sessionOptions)
