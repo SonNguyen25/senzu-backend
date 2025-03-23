912e3d2d807db39e2e68c65cc4e461d78c8e2149
sonnguyen25 <ssbaoson@gmail.com>
2025-03-23 03:33:51 -0400

    fix cors

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 // console.log(process.env.OPENAI_API_KEY);
-app.use("*", cors({
+app.options("*", cors({
     credentials: true,
     origin: [process.env.FRONTEND_URL, "http://localhost:3000", process.env.HTTP_SERVER_DOMAIN, 'https://senzusocial.netlify.app'],
     methods: ["GET","POST","PUT","DELETE","OPTIONS"],
